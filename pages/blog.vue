3253b78e0d3d81471f674f7e996a5bdc210c0c99
Brad Fitzwater <brad.fitzwater@gmail.com>
2020-09-10 21:50:34 -0400

    blog page

---
+++ pages/blog.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <h1>Blog Content</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style lang="postcss">
+.container {
+  @apply min-h-screen flex flex-col mx-auto;
+}
+</style>
