3f82ecce39bce7f08eb123c152a9e9b9451e89fd
fitz <nanunderflow@gmail.com>
2020-09-10 22:21:21 -0400

    adding test blog post

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <nuxt-content :document="article" /> 
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  async asyncData({ $content, params }) {
+    const article = await $content('articles', params.slug).fetch();
+    return { article };
+  } 
+})
+</script>
+
+<style lang="postcss">
+.container {
+  @apply min-h-screen flex flex-col mx-auto;
+}
+</style>
