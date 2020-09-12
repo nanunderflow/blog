0a02707a004b5dfa2cda2ac555f7a7e44920e78d
fitz <nanunderflow@gmail.com>
2020-09-11 22:07:40 -0400

    wip

--- content/articles/test.md
+++ content/articles/test.md
@@ -1,6 +1,7 @@
 ---
 title: My first Blog Post
 description: Learning how to use @nuxt/content to create a blog
+published: Sep 11, 2020
 ---
 
 Welcome to my first blog post using content module
--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="container">
-    <h1>{{ article.title }}</h1>
-    <p>{{ article.description }}</p>
-    <nuxt-content :document="article" /> 
+    <header>
+      <h1>{{ article.title }}</h1>
+    </header>
+    <div class="description">
+      <p>
+        {{ article.description }}
+      </p>
+    </div>
+    <div class="body">
+      <nuxt-content :document="article" />
+    </div>
+    <div class="published">
+      {{ article.published }}
+    </div>
   </div>
 </template>
 
@@ -21,4 +32,58 @@ export default Vue.extend({
 .container {
   @apply min-h-screen flex flex-col mx-auto;
 }
+
+header {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 1rem;
+}
+
+h1 {
+  font-weight: 300;
+  font-size: 5rem;
+  line-height: 1.2;
+}
+
+.description {
+  display: flex;
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+  justify-content: center;
+}
+
+.description p {
+  width: auto;
+  max-width: 90%;
+  font-weight: bolder;
+  font-size: 1.8rem;
+  line-height: 1.2;
+}
+
+@screen md {
+  .description p {
+    width: auto;
+    max-width: 70%;
+  }
+}
+
+.published {
+  margin-bottom: 1rem;
+  text-align: center;
+}
+
+.body {
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+}
+
+.body p {
+  font-size: 1.6rem;
+  font-weight: 500;
+  line-height: 1.2;
+  margin-bottom: 1rem;
+}
+
 </style>
